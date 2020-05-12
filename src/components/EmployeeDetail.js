75b88e3e84f99aaf60567189f3b9b7776b178474
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-12 17:16:54 -0500

    added row and employee detail

---
+++ src/components/EmployeeDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function EmployeeDetail(props) {
+    return (
+        <div className="text-center">
+            <img alt={props.title} className= "img-fluid" src={props.src} style={{margin:"0 auto"}}/>
+            <h3>Location: {props.location}</h3>
+            <h3>Email: {props.email}</h3>
+            <h3>Username: {props.username}</h3>
+        </div>
+    )
+}
