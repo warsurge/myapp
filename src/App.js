77d321cf2639b255bc84f247b889b65ef8ba25c0
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-13 16:57:28 -0500

    added search, added filter, fixed pictures

--- src/App.js
+++ src/App.js
@@ -4,29 +4,64 @@ import Wrapper from "./components/Wrapper"
 import Title from "./components/Title"
 import employees from "./employees.json"
 
+
 class App extends Component {
-  state ={
+  state = {
     employees
   };
 
-  render(){
-  return (
-    <Wrapper>
-      <Title>Employee List</Title>
-      {this.state.employees.map(employee =>(
-        <EmployeeCard
-        id= {employee.id}
-        name={employee.name}
-        image={employee.image}
-        email={employee.email}
-        username={employee.username}
-        phone={employee.phone}
-        occupation = {employee.occupation}
-        location= {employee.location}
-        />
-      ))}
-    </Wrapper>
-  );
+  handleNameSort = (name, order) => {
+    this.setState({ employees: this.state.employees.sort((a, b) => (a.name[name] > b.name[name] ? order : -order)) })
+  }
+
+  handleSort = (name, order) => {
+    this.setState({ employees: this.state.employees.sort((a, b) => (a[name] > b[name] ? order : -order)) })
+  }
+
+  searchFilter = (name) => {
+    this.setState({
+      employees: employees.filter(employee => {
+        return employee.name.toLowerCase().includes(name.toLowerCase())
+          || employee.occupation.toLowerCase().includes(name.toLowerCase())
+          || employee.username.toLowerCase().includes(name.toLowerCase());
+      })
+    })
+  }
+
+  render() {
+    return (
+
+      <Wrapper>
+
+        <Title>Employee List
+        <div>
+            <input type="text" placeholder="Search" onChange={(e) => this.searchFilter(e.target.value)} />
+            <div className="h4 my-0 py-2">
+              <div className="col">
+                <div className="d-inline">
+                  Type Name, Username, or Occupation to Search
+              </div>
+              </div>
+            </div>
+          </div>
+        </Title>
+
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            picture={employee.picture}
+            email={employee.email}
+            username={employee.username}
+            phone={employee.phone}
+            occupation={employee.occupation}
+            location={employee.location}
+          />
+        ))}
+      </Wrapper>
+
+    );
   }
 }
 
--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -5,7 +5,7 @@ function EmployeeCard(props){
     return (
         <div className = "card">
             <div className= "img-container">
-                <img alt ={props.name} src={props.image} />
+                <img alt ={props.name} src={props.picture} />
             </div>
 
             <div className ="content">
