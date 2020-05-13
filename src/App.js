9ccc57b82d7e6218a71362fbad61956cb7d9b84f
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-12 21:34:48 -0500

    edited json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import EmployeeContainer from "./components/EmployeeContainer"
-function App() {
+import React, { Component } from 'react';
+import EmployeeCard from "./components/EmployeeCard"
+import Wrapper from "./components/Wrapper"
+import Title from "./components/Title"
+import employees from "./employees.json"
+
+class App extends Component {
+  state ={
+    employees
+  };
+
+  render(){
   return (
-    <EmployeeContainer />
+    <Wrapper>
+      <Title>Employee List</Title>
+      {this.state.employees.map(employee =>(
+        <EmployeeCard
+        id= {employee.id}
+        key={employee.id}
+        name={employee.name}
+        image={employee.image}
+        occupation = {employee.occupation}
+        location= {employee.location}
+        />
+      ))}
+    </Wrapper>
   );
+  }
 }
 
 export default App;
