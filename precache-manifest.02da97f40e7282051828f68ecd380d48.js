c8d99c831549e9357e0e3c8c2edef9e37a066bed
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-14 14:21:14 -0500

    Updates

---
+++ precache-manifest.02da97f40e7282051828f68ecd380d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6afad0a7425bf8924a8fe34cb0643f",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "83fd8f0db677d056110e",
+    "url": "/myapp/static/css/main.2acc0726.chunk.css"
+  },
+  {
+    "revision": "877c380a4baf3445711f",
+    "url": "/myapp/static/js/2.e9ef52b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.e9ef52b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83fd8f0db677d056110e",
+    "url": "/myapp/static/js/main.bca3e091.chunk.js"
+  },
+  {
+    "revision": "26eb195be2ce8022641c",
+    "url": "/myapp/static/js/runtime-main.47644e1d.js"
+  }
+]);
